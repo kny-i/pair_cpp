2df0b006e8e493525712c6532500ad1e06a324c9
小平波琉 <h.kopei.89315167@icloud.com>
2022-08-23 15:15:23 +0900

    .gitignore追加してBrainのheaderfile入れた

---
+++ cpp04/ex01/Brain.hpp
@@ -0,0 +1,25 @@
+//
+// Created by 小平波琉 on 2022/08/21.
+//
+
+#ifndef CPP_BRAIN_HPP
+#define CPP_BRAIN_HPP
+#include <iostream>
+#include <sstream>
+
+class Brain {
+public:
+	Brain();
+	Brain(std::string type);
+	virtual ~Brain();
+	Brain(const Brain &rhs);
+	Brain &operator=(const Brain &rhs);
+
+	const std::string *getIdeas() const;
+	void printIdeas() const;
+
+private:
+	std::string ideas[100];
+};
+
+#endif //CPP_BRAIN_HPP
