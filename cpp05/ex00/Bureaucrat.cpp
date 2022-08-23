1eadf905b9938a092fe40eebaade50047776726d
小平波琉 <h.kopei.89315167@icloud.com>
2022-08-23 21:58:36 +0900

    start bureaucrat

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{
+	std::cerr << "(Bureaucrat constructor called)" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cerr << "(Bureaucrat Destructor called)" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &rhs)
+{
+	*this = rhs;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs) {
+	if (this != &rhs) {
+		grade_ = rhs.getGrade();
+	}
+	return *this;
+}
+
+const std::string &Bureaucrat::getName() const {
+	return name_;
+}
+
+size_t Bureaucrat::getGrade() const {
+	return grade_;
+}
+
+void Bureaucrat::setGrade(size_t grade) {
+	grade_ = grade;
+}
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -5,4 +5,21 @@
 #ifndef CPP_BUREAUCRAT_HPP
 #define CPP_BUREAUCRAT_HPP
 
+#include <iostream>
+
+class Bureaucrat {
+public:
+	Bureaucrat();
+	Bureaucrat(const Bureaucrat &rhs);
+	~Bureaucrat();
+	Bureaucrat &operator=(const Bureaucrat &rhs);
+	const std::string &getName() const;
+	size_t getGrade() const;
+	void setGrade(size_t grade);
+
+private:
+	const std::string name_;
+	size_t grade_;
+};
+
 #endif //CPP_BUREAUCRAT_HPP
