d189279c4d12937962580775401f7be65b4a4e6f
小平波琉 <h.kopei.89315167@icloud.com>
2022-08-24 16:00:41 +0900

    main OK

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,24 @@
 
 int main()
 {
-	Bureaucrat bureaucrat;
-	std::cout << bureaucrat << std::endl;
+	try {
+		Bureaucrat bureaucrat;
+		std::cout << bureaucrat << std::endl;
+		bureaucrat.gradeDown();
+		std::cout << bureaucrat << std::endl;
+		bureaucrat.gradeUp();
+		std::cout << bureaucrat << std::endl;
+		for (size_t i = 0; i < 43; i++) {
+			bureaucrat.gradeUp();
+		}
+		std::cout << bureaucrat << std::endl;
+
+	} catch (GradeTooHighException e) {
+		std::cerr << "GRADE IS TOO HIGH " << e.what() << std::endl;
+	} catch (GradeTooLowException e) {
+		std::cerr << "GRADE IS TOO LOW " << e.what() << std::endl;
+	} catch (std::exception e) {
+		std::cerr << "ERROR" << e.what() << std::endl;
+	}
 }
 
