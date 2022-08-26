69203d9776d4612b6c98c25a07c91726b4edf192
小平波琉 <h.kopei.89315167@icloud.com>
2022-08-27 03:57:16 +0900

    Intern canonical form OK

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -1,1 +1,25 @@
 #include "Intern.hpp"
+
+Intern::Intern()
+{
+	std::cerr << "(Intern constructor called)" << std::endl;
+}
+
+Intern::~Intern()
+{
+	std::cerr << "(Intern destructor called)" << std::endl;
+}
+
+Intern::Intern(const Intern &rhs)
+{
+	std::cerr << "(Copy constructor called)" << std::endl;
+}
+
+Intern &Intern::operator=(const Intern &rhs) {
+	if (this != &rhs) {
+
+	}
+	return *this;
+}
+
+
--- cpp05/ex03/Intern.hpp
+++ cpp05/ex03/Intern.hpp
@@ -2,5 +2,13 @@
 
 #ifndef CPP_INTERN_HPP
 #define CPP_INTERN_HPP
+#include <iostream>
+
+class Intern {
+	Intern();
+	~Intern();
+	Intern(const Intern &rhs);
+	Intern &operator=(const Intern &rhs);
+};
 
 #endif //CPP_INTERN_HPP
