6a494701b9c710b4cb8554a502626575de7bb387
小平波琉 <h.kopei.89315167@icloud.com>
2022-08-31 06:49:27 +0900

    start cpp07

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,16 @@
+#include "whatever.hpp"
+
+int main()
+{
+	int a = 42, b = 24;
+
+	std::cout << "a = 42, b = 24" << std::endl;
+	std::cout << "max(a, b) == "
+			  << max(a, b)
+			  << std::endl;
+
+	std::cout << "min(a, b) == "
+			  << min(a, b)
+			  << std::endl;
+	//swap
+}
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -5,4 +5,33 @@
 #ifndef CPP_WHATEVER_HPP
 #define CPP_WHATEVER_HPP
 
+#include <iostream>
+#include "whatever.hpp"
+
+template<typename T>
+T min(T a, T b) {
+	if (a < b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+template<typename T>
+T max(T a, T b) {
+	if (a < b) {
+		return b;
+	} else {
+		return a;
+	}
+}
+
+template <typename T>
+void swap(T &a, T &b) {
+	T tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
 #endif //CPP_WHATEVER_HPP
