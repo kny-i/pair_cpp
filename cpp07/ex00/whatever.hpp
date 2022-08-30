2b3b09a4ec5c94cb4427df4b279ef9eeb4971ddd
小平波琉 <h.kopei.89315167@icloud.com>
2022-08-31 07:52:28 +0900

    whatever end

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -9,7 +9,7 @@
 #include "whatever.hpp"
 
 template<typename T>
-T min(T a, T b) {
+T min(const T a, const T b) {
 	if (a < b) {
 		return a;
 	} else {
@@ -18,11 +18,11 @@ T min(T a, T b) {
 }
 
 template<typename T>
-T max(T a, T b) {
-	if (a < b) {
-		return b;
-	} else {
+T max(const T a, const T b) {
+	if (a > b) {
 		return a;
+	} else {
+		return b;
 	}
 }
 
