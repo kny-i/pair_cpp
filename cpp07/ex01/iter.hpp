392a97ed6d9e918642d818dbe774f706fa6704f5
小平波琉 <h.kopei.89315167@icloud.com>
2022-08-31 08:14:16 +0900

    finish cpp07ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -4,5 +4,26 @@
 
 #ifndef CPP_ITER_HPP
 #define CPP_ITER_HPP
+#include <iostream>
+
+template <typename T>
+void iter(T *array, size_t len, void (*f)(T &)) {
+	if (array == NULL || f == NULL) {
+		return ;
+	}
+	for (size_t i = 0; i < len; i++) {
+		f(array[i]);
+	}
+}
+
+/* template for test */
+template <typename T>
+void print(const T &value) {
+	std::cout << value << std::endl;
+}
+template <typename T>
+void print(const T value) {
+	std::cout << value << " ";
+}
 
 #endif //CPP_ITER_HPP
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,3 +1,38 @@
 //
 // Created by 小平波琉 on 2022/08/04.
 //
+#include "iter.hpp"
+
+int main()
+{
+	{
+		std::cout << "[INT CASE]" << std::endl;
+		int array[] = {1, 2, 3};
+		::iter(array, sizeof(array) / sizeof(array[0]), print);
+	}
+
+	{
+		std::cout << std::endl << "[DOUBLE CASE]" << std::endl;
+		double array[] = {1.23, 4.56, 7.89};
+		::iter(array, sizeof(array) / sizeof(array[0]), print);
+	}
+
+	{
+		std::cout << std::endl << "[DOUBLE CASE]" << std::endl;
+		char array[] =
+				{'4',
+				 '2',
+				 'T',
+				 'o',
+				 'k',
+				 'y',
+				 'o',
+				 '!', '\0'};
+		::iter(array, sizeof(array) / sizeof(array[0]), print);
+	}
+	{
+		std::cout << std::endl << "[STRING CASE]" << std::endl;
+		std::string array[] = {"NOP", "SATAHARU",  "BUNJIRO"};
+		::iter(array, sizeof(array) / sizeof(array[0]), print);
+	}
+}
