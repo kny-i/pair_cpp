dba353c82072c417ddca3a4b844fcac6ed9b48d6
kny-i <kny_i@keio.jp>
2022-09-02 08:20:47 +0900

    wip

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -2,4 +2,45 @@
 #ifndef CPP_MUTANTSTACK_HPP
 #define CPP_MUTANTSTACK_HPP
 
+
+#include <iostream>
+#include <stack>
+
+template <class T>
+class MutantStack : public std::stack<T>
+{
+public:
+	typedef std::stack<T> stack;
+	typedef typename stack::container_type container;
+	typedef typename container::iterator iterator;
+
+	MutantStack() : stack()
+	{
+		std::cout << "Default Constructor called" << std::endl;
+	}
+	MutantStack(const stack &rhs) : stack(rhs)
+	{
+		std::cout << "Copy Constructor called" << std::endl;
+	}
+	~MutantStack()
+	{
+		std::cout << "Deconstuctor called" << std::endl;
+	}
+	stack &operator=(const stack &rhs)
+	{
+		if (*this != rhs)
+			*this = rhs;
+		return (*this);
+	}
+
+	iterator begin()
+	{
+		return (stack::c.begin());
+	}
+
+	iterator end()
+	{
+		return (stack::c.end());
+	}
+};
 #endif //CPP_MUTANTSTACK_HPP
---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,50 @@
+#include "MutantStack.hpp"
+
+int	main()
+{
+	std::cout << "---testSubject---" << std::endl;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...] 
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	std::cout << std::endl;
+
+	std::cout << "---testSubject---" << std::endl;
+	MutantStack<std::string> sstack;
+	sstack.push("hoge");
+	sstack.push("piyo");
+	std::cout << sstack.top() << std::endl;
+	sstack.pop();
+	std::cout << sstack.size() << std::endl;
+	sstack.push("foo");
+	sstack.push("fuga");
+	sstack.push("nop");
+	//[...]
+	sstack.push("AKO");
+	MutantStack<std::string>::iterator a = sstack.begin();
+	MutantStack<std::string>::iterator b = sstack.end();
+	++a;
+	--a;
+	while (a!= b) {
+		std::cout << *a<< std::endl;
+		++a;
+	}
+	std::stack<std::string> c(sstack);
+	std::cout << std::endl;
+}
