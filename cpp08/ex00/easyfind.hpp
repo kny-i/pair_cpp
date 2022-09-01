c8105bbacebcf1fda451683b8556d90cbf929e6e
kny-i <kny_i@keio.jp>
2022-09-02 04:22:14 +0900

    wip

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,5 +1,21 @@
-
 #ifndef CPP_EASYFIND_HPP
 #define CPP_EASYFIND_HPP
+
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <exception>
+
+template< typename T>
+typename T::iterator easyfind(T &in, int i)
+{
+	typename T::iterator	it;
+	it = find(in.begin(), in.end(), i);
+	if (it == in.end())
+	{
+		throw (std::exception());
+	}
+	return (it);
+}
 
 #endif //CPP_EASYFIND_HPP
